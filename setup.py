a4c460cc67494ed6550211bc6eb1781ec1623408
Roni Shamai <ronishamai98@gmail.com>
2022-08-04 15:38:53 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name='mykmeanssp',
+    version='0.1.0',
+    author='Ayala Koslowsky, Roni Shamai',
+    author_email='ronishamai@mail.tau.ac.il',
+    description='K-means++ algorithm, which is used to choose initial centroids for the K-means algorithm',
+    install_requires=['invoke'],
+    packages=find_packages(),
+    license='GPL-2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    ext_modules=[
+        Extension(
+            'mykmeanssp',
+            ['kmeans.c']
+        )
+
+    ]
+
+)
