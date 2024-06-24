4c529a149d1a9b5dfe197ddb0b1ebe1ede2751ba
ronishamai <ronishamai@mail.tau.ac.il>
2024-06-24 12:48:19 +0300

    K-means++ algorithm files

--- comp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script to compile and execute a c program
-gcc -ansi -Wall -Wextra -Werror -pedantic-errors spkmeans.c -lm -o spkmeans
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages, Extension
 
 setup(
-    name='spkmeans',
+    name='mykmeanssp',
     version='0.1.0',
     author='Ayala Koslowsky, Roni Shamai',
     author_email='ronishamai@mail.tau.ac.il',
-    description='the normalized spectral clustering algorithm',
+    description='K-means++ algorithm, which is used to choose initial centroids for the K-means algorithm',
     install_requires=['invoke'],
-    packages=find_packages(),
+    package=find_packages(),
     license='GPL-2',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -18,8 +18,8 @@ setup(
     ],
     ext_modules=[
         Extension(
-            'spkmeans',
-            ['spkmeansmodule.c','spkmeans.c']
+            'mykmeanssp',
+            ['kmeans.c']
         )
 
     ]
